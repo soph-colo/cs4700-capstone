74f3901d4c5d5f81df3bc91a106f46a03ad0c9af
Sophia Colonello <sophiacolonello@gmail.com>
2024-10-29 10:49:11 -0400

    commented with minor changes

--- neurodroneFlight1.py
+++ neurodroneFlight1.py
@@ -1,14 +1,20 @@
+
+# imports
 import asyncio
 from itertools import count
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import AsyncIOOSCUDPServer
-
 from codrone_edu.drone import *
 
+
+# initalize drone object
 drone = Drone()
 
+# global variables to store live data and record the history of the flight
 data_all = [] #collects live data
 flight_history = [] #collects executed drone commands
+
+
 '''
 Drone Commands:
     * 0: neutral
@@ -16,76 +22,93 @@ Drone Commands:
     * -1: pull (backward)
 '''
 
-
+# the function that takes the data from headset and adds it to our live data array
 def process_data(address, *args):
     print(f"Received OSC message: {address}, {args}")
     data_all.append(address)
 
+# async method that handles the flight of the drone while also still receiving data
+async def fly_drone():
 
-async def run_script():
+    # variables
     neutral_count = 0  # 10 or more consecutive neutrals end the program
-    # pair it
+    n = True
+
+    # using drone object, pair and have drone take off
     drone.pair()
     drone.takeoff()
 
-    n = 'y'
+    # while loop to keep drone flying until 10 or more consecutive neutrals end the program
+    while n:
 
-    while n == 'y':
-
+        # TODO: remove these eventually
         print("Running script")
         print("data here", data_all)
 
+        # only filter the data if the length of our raw data is greater than one
         if len(data_all) > 1:
 
-            data_filtered = data_all.copy() #pulls the most recent headset outputs
+            # pull the most recent headset output and filter
+            data_filtered = data_all.copy() 
 
-            #Identifies the most frequent output as the drone command
+            # identifies the most frequent output as the drone command
             drone_command = max(set(data_filtered), key=data_filtered.count)
 
-            # Sends the command to the drone
-            if (drone_command == "/com/neutral"):
-                neutral_count += 1  # update the consecutive neutral count
-                flight_history.append(0)  # 0 being neutral
-                print("Remain at neutral")
-            elif (drone_command == "/com/push"):
-                neutral_count = 0  # resetting neutral count
-                flight_history.append(1)  # 1 being push
-                print("Move forward")
-                # Instigating flight
-                drone.set_pitch(30)
-                drone.move(1)
-                drone.set_pitch(0)
-                drone.move(1)
-            elif (drone_command == "/com/pull"):
-                neutral_count = 0  # resetting neutral count
-                flight_history.append(-1)  # -1 being pull
-                print("Move backward")
-                # Instigating flight
-                drone.set_pitch(-30)
-                drone.move(1)
-                drone.set_pitch(0)
-                drone.move(1)
-            else:
-                neutral_count = 0  # resetting neutral count
-                print("Data unclear, more input required")
+            # sends the command to the drone
+            match drone_command:
+                case "/com/neutral":
 
-            endpoint = len(data_filtered) - 2 #sets the closing index for removing the processed data
+                    # update consecutive neutral count
+                    neutral_count += 1  
+                    flight_history.append(0)  # 0 being neutral
+                    print("Remain at neutral")  # TODO: remove these prints eventually
+                case "/com/push":
 
-            # deleting handled data from the live stream
-            # leaves two elements so the list is not empty
+                    neutral_count = 0  # resetting neutral count
+                    flight_history.append(1)  # 1 being push
+                    print("Move forward")
+
+                    # moving the drone forward one unit (30 pitch per second)
+                    drone.set_pitch(30)
+                    drone.move(1)
+                    drone.set_pitch(0)
+                    drone.move(1)
+                case "/com/pull":
+
+                    neutral_count = 0  # resetting neutral count
+                    flight_history.append(-1)  # -1 being pull
+                    print("Move backward")
+
+                    # moving the drone backword one unit (30 pitch per second)
+                    drone.set_pitch(-30)
+                    drone.move(1)
+                    drone.set_pitch(0)
+                    drone.move(1)
+                case _:
+
+                    neutral_count = 0  # resetting neutral count
+                    print("Data unclear, more input required")
+
+            # sets the closing index for removing the processed data
+            endpoint = len(data_filtered) - 2 
+
+            # deleting handled data from the live stream (leaves two elements so the list is not empty)
             del data_all[0:endpoint]
 
-            #Ending the flight
+            # end the flight
             if (neutral_count >= 10):
                 drone.land()
                 drone.close()
-                n = 'n'
+                n = False
 
-        # drone flight here
+        # pause execution of while loop for one second so more data can be collected
         await asyncio.sleep(1)
 
 
+# main method that initalizes the communication between the headset and computer
 async def main():
+
+    # dispatcher object to handle OSC communication
     dispatcher = Dispatcher()
     dispatcher.map("/com/neutral", process_data)
     dispatcher.map("/com/push", process_data)
@@ -94,11 +117,14 @@ async def main():
     dispatcher.map("/com/right", process_data)
     dispatcher.map("/com/lift", process_data)
 
+    # server intialization with parameters to received OSC to this device
     server = AsyncIOOSCUDPServer(("127.0.0.1", 8000), dispatcher, asyncio.get_event_loop())
     transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
 
-    script_task = asyncio.create_task(run_script())
+    # creat task that needs completed asyncronessly 
+    script_task = asyncio.create_task(fly_drone())
 
+    # try to perform this action until it is exited
     try:
         await asyncio.gather(script_task)
     finally:
